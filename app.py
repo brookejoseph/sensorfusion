4012d38bf7ac87b2eb8edf0e93199880ae0c2234
Brooke Joseph <brookecoco123123@gmail.com>
2024-05-13 19:17:02 -0400

    fixed up the ui

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from pykalman import KalmanFilter
 
+df1 = df2 = None
 
+st.markdown("<h1 style='text-align: center; color: black; margin-bottom: 50px;'>Fusion your data!</h1>", unsafe_allow_html=True)
+
+accelemeter = st.file_uploader("Upload your accelemeter data", type=["csv"])
+gyroscope = st.file_uploader("Upload your gyroscope data", type=["csv"])
+
+if accelemeter and gyroscope:
+    df1 = pd.read_csv(accelemeter)
+    df2 = pd.read_csv(gyroscope)
+
+    merged_data = pd.merge(df1, df2, on='x', how='inner')
+    
